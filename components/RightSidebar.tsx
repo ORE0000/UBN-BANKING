53f8cc6ff1d0564a5513e6c551ad5e7523bc2a9c
ORE0000 <ashutoshpant.855@gmail.com>
2024-10-20 13:09:01 +0530

    sign in and sign up changes

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -12,12 +12,12 @@ const RightSidebar = ({user,transactions,banks} :RightSidebarProps) => {
             <div className='profile'>
                 <div className='profile-img'>
                     <span className='text-5xl font-bold text-blue-500'>
-                        {user.firstName[0]}
+                        {user.name[0]}
                     </span>
                 </div>
                 <div className='profile-details'>
                     <h1 className='profile-name'>
-                        {user.firstName}{user.lastName}
+                        {user.name}
                     </h1>
                     <p className='profile-email'>
                         {user.email}
@@ -41,12 +41,12 @@ const RightSidebar = ({user,transactions,banks} :RightSidebarProps) => {
                 </Link>
             </div>
             {banks?.length > 0 && (
-                <div className='relative flex flex-1 flex-col items-centre justify-centre gap-5'>
+                <div className='relative flex flex-1 flex-col items-center justify-centre gap-5'>
                     <div className='relative z-10'>
                         <BankCard
                         key ={banks[0].$id}
                         account={banks[0]}
-                        userName={`${user.firstName} ${user.lastName}`}
+                        userName={user.name}
                         showBalance={false}
                         />
                     </div>
@@ -55,7 +55,7 @@ const RightSidebar = ({user,transactions,banks} :RightSidebarProps) => {
                         <BankCard
                         key={banks[1].$id}
                         account={banks[1]}
-                        userName={`${user.firstName} ${user.lastName}`}
+                        userName={user.name}
                         showBalance={false}
                         />
                     </div>
